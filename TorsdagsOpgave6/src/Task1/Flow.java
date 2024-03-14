51cc647fdf55bda20d38bbd5746e30a3788da38a
MickeJDengaard <cph-md405@cphbusiness.dk>
2024-03-14 13:46:51 +0100

    Task1

---
+++ TorsdagsOpgave6/src/Task1/Flow.java
@@ -0,0 +1,29 @@
+package Task1;
+
+public class Flow {
+
+   public void methodA(){
+       System.out.print("J");
+       System.out.print("a");
+       System.out.print("v");
+       System.out.print("a");
+    }
+
+    public void methodB(){
+        System.out.print(" E");
+        System.out.print("r ");
+    }
+
+    public void methodC(){
+        System.out.print("S");
+        System.out.print("j");
+        System.out.print("o");
+    }
+
+    public void methodD(){
+        System.out.print("v");
+        System.out.print("t");
+    }
+
+
+}
---
+++ TorsdagsOpgave6/src/Task1/Main.java
@@ -0,0 +1,15 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args){
+        Flow f = new Flow();
+
+        f.methodA();
+        f.methodB();
+        f.methodC();
+        f.methodD();
+
+    }
+
+
+}
