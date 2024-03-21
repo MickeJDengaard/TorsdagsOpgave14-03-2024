e4f8428da85d5059a2af4e2ae57a6d57c7edb4e7
MickeJDengaard <158359543+MickeJDengaard@users.noreply.github.com>
2024-03-21 20:44:17 +0100

    Delete SP2/src directory

--- SP2/src/Car.java
+++
@@ -1,9 +0,0 @@
-public interface Car {
-    abstract String getRegistrationNumber();
-    String getMake();
-    String getModel();
-    int getNumberOfDoors();
-    int getRegistrationFee();
-
-
-}
